b421e93e20e3d808c9e075442a757f8883b1dfb5
Mitul Desai <mitul.desai@outlook.com>
2025-01-11 17:02:26 +0530

    feat: Firebase analytics

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2370587878"
+    serviceWorkerVersion: "1805097442"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "ed312e2149f3838d2d9607cd6d5b17ee",
+const RESOURCES = {"flutter_bootstrap.js": "ae105a91e04ae45ee09f74626e8edfd1",
 "version.json": "9b818ca9511483c901bed1545384376c",
 "index.html": "6b2eb6c1b58079d5cfd0bcce2d614cae",
 "/": "6b2eb6c1b58079d5cfd0bcce2d614cae",
-"main.dart.js": "9cecd82ec1bd2e69642c062944dc5110",
+"main.dart.js": "87b26fa48040de2bed26af21abafa873",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "8d88c2fdcd59e8999e99c9eaec6edd2a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "ed312e2149f3838d2d9607cd6d5b17ee",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "e2b9103aaafaf8ed9f90576c9d9a1341",
 "assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
-"assets/NOTICES": "53af9316bbe1e8ada4ef58f0b3e9e087",
+"assets/NOTICES": "54e378b8430984c9c7df86de1065a2a3",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/AssetManifest.bin.json": "4446c963dde6091a6b6de4cb1ae02947",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
